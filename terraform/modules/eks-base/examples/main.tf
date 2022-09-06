6f4dcc7e607c60c432fa805c7aa2129892ec0d3e
Liviu Damian <liviudm@liviudm.com>
2022-09-06 10:28:40 +0800

    Fix example

--- terraform/modules/eks-base/examples/main.tf
+++ terraform/modules/eks-base/examples/main.tf
@@ -17,39 +17,39 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = module.eks_base.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_base.cluster_certificate_authority_data)
 
   exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+      args        = ["eks", "get-token", "--cluster-name", module.eks_base.cluster_id]
     }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = module.eks_base.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_base.cluster_certificate_authority_data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+      args        = ["eks", "get-token", "--cluster-name", module.eks_base.cluster_id]
     }
   }
 }
 
 provider "kubectl" {
   apply_retry_count      = 5
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = module.eks_base.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_base.cluster_certificate_authority_data)
   load_config_file       = false
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+    args        = ["eks", "get-token", "--cluster-name", module.eks_base.cluster_id]
   }
 }
 
@@ -57,11 +57,11 @@ provider "kubectl" {
 # Data resources
 #---------------------------------------------------------------
 data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_id
+  name = module.eks_base.cluster_id
 }
 
 data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_id
+  name = module.eks_base.cluster_id
 }
 
 #---------------------------------------------------------------
