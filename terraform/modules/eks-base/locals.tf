8563508c53873e242ac4ffec11375c05d3473407
Liviu Damian <liviudm@liviudm.com>
2022-09-28 12:19:50 +0300

    Upgrade Karpenter to 0.16.3
    Signed-off-by: Liviu Damian <liviudm@liviudm.com>

--- terraform/modules/eks-base/locals.tf
+++ terraform/modules/eks-base/locals.tf
@@ -5,7 +5,7 @@ locals {
     "kube-proxy"         = "v1.23.7-eksbuild.1",
     "aws-ebs-csi-driver" = "v1.11.2-eksbuild.1",
     "vpc-cni"            = "v1.11.3-eksbuild.1",
-    "karpenter"          = "0.16.2"
+    "karpenter"          = "0.16.3"
   }
   github_known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
 }
